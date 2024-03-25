3b8e2d1b3bd20aa8bb42cf2f44055ef4035b364b
Abhinav-SU <abbajpai@syr.edu>
2024-03-25 17:32:22 -0400

    test-user

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V1
+          fitness app V1
         </a>
       </header>
     </div>
