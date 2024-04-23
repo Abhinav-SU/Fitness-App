8bb6ee7fe4dc9efa649b9a5aca56e271548b994b
miteshgaonkar <gaonkar3618@gmail.com>
2024-04-23 08:04:50 +0530

    Backend implemention

--- firebase.js
+++ firebase.js
@@ -2,11 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth, EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, serverTimestamp, collection } from "firebase/firestore";
 
-console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -15,11 +14,13 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-
 const db = new getFirestore(app);
 
-
-
+export const database = {
+  exercises: collection(db, "exercises"),
+  workouts: collection(db, "workouts"),
+  getCurrentTimestamp: serverTimestamp,
+};
 
 export const auth = getAuth();
 
@@ -29,4 +30,4 @@ export const emailProvider = new EmailAuthProvider();
 
 export default app;
 
-
+export { db };
